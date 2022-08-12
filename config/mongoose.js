3264e763c03b675ccc48f8250fd83e92e8aa17dc
unknown <vikas.chaudhary.met19@itbhu.ac.in>
2022-08-13 04:04:17 +0530

    RealTime Chat App

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://PawanKumarMetaChat:rvZqDDmeMGJl2OCe@cluster0.ikjranw.mongodb.net/messages?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://VikasChaudhary:<password>@cluster0.hb3iamh.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('connected');
